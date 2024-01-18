87308b27b507ebb1e7e0bcf1199075dd3f64e827
TobyWhittome <Toby.Whittome@outlook.com>
2024-01-18 17:45:27 +0000

    CEO duality added, has issues

--- create_dataset.py
+++ create_dataset.py
@@ -28,7 +28,8 @@ def output_excel_file(database, filename):
   database.to_excel(excel_file_path, index=False)
 
 
-def count_committees(dataframe):
+def count_committees():
+  dataframe = pd.DataFrame(data=(db.raw_sql(f"SELECT TICKER, AUDIT_MEMBERSHIP, CG_MEMBERSHIP, COMP_MEMBERSHIP, NOM_MEMBERSHIP FROM risk.rmdirectors WHERE YEAR BETWEEN '{lastyear}' AND '{thisyear}' AND TICKER IN {SP500List}")))
   companyCounted = set()
   com_data = []
   for index, row in dataframe.iterrows():
@@ -38,34 +39,56 @@ def count_committees(dataframe):
       companyCounted.add(row['ticker'])
   return pd.DataFrame(com_data)
 
+
+def is_CEO_Dual():
+  dataframe = pd.DataFrame(data=(db.raw_sql(f"SELECT TICKER, EMPLOYMENT_CEO, EMPLOYMENT_CHAIRMAN FROM risk.rmdirectors WHERE YEAR BETWEEN '{lastyear}' AND '{thisyear}' AND TICKER IN {SP500List}")))
+  com_data = []
+  for index, row in dataframe.iterrows():
+    if row['employment_ceo'] == 'Yes':
+      if row['employment_chairman'] == 'Yes':
+        com_data.append({'ticker': row['ticker'], 'CEODuality': True})
+      else:
+        com_data.append({'ticker': row['ticker'], 'CEODuality': False})
+  print(pd.DataFrame(com_data))
+  return pd.DataFrame(com_data)
+                
+
 def read_in_data_from_wrds():
+
+  #Company Database
   query = f'''
-  SELECT g.TICKER, g.MEETINGDATE, g.MTGMONTH, g.YEAR, g.DUALCLASS, e.NUMMTGS, e.YEAR
+  SELECT g.TICKER, g.YEAR, g.DUALCLASS, e.NUMMTGS, e.YEAR
   FROM risk.rmgovernance g
-  JOIN comp_execucomp.codirfin e ON g.TICKER = e.TICKER AND e.YEAR = g.YEAR
-  WHERE g.YEAR BETWEEN 2023 AND 2024 AND g.TICKER IN {SP500List}
+  JOIN comp_execucomp.codirfin e 
+  ON g.TICKER = e.TICKER AND e.YEAR = g.YEAR
+  WHERE g.YEAR BETWEEN '{lastyear}' AND '{thisyear}' AND g.TICKER IN {SP500List}
   '''
   merged_data = pd.DataFrame(data=(db.raw_sql(query)))
   #print(merged_data)
 
+
   #Size (org summary) & Diversity ... need to add Annualreportdate BETWEEN 2023-01-01 AND 2024-01-15
   OrgSummary = pd.DataFrame(data=(db.raw_sql(f"SELECT Ticker, NumberDirectors, GenderRatio, NationalityMix, Annualreportdate FROM boardex.na_wrds_org_summary WHERE Annualreportdate BETWEEN '{year_ago_date}' AND '{today_date}' AND TICKER IN {SP500List}")))
   #print(OrgSummary)
-  #%Independent Directors & Shares held & CEO Duality & Number of committees & Voting type
-  DirectorsUS = pd.DataFrame(data=(db.raw_sql(f"SELECT TICKER, CLASSIFICATION, NUM_OF_SHARES, EMPLOYMENT_CEO, EMPLOYMENT_CHAIRMAN, AUDIT_MEMBERSHIP, CG_MEMBERSHIP, COMP_MEMBERSHIP, NOM_MEMBERSHIP, OWNLESS1, PCNT_CTRL_VOTINGPOWER FROM risk.rmdirectors WHERE YEAR BETWEEN '{lastyear}' AND '{thisyear}' AND TICKER IN {SP500List}")))
-  count_committees(DirectorsUS)
-  Governance = pd.DataFrame(data=(db.raw_sql(f"SELECT TICKER, DUALCLASS FROM risk.rmgovernance WHERE YEAR BETWEEN '{lastyear}' AND '{thisyear}' AND TICKER IN {SP500List}")))
-  print(Governance)
-  Execucomp = pd.DataFrame(data=(db.raw_sql(f"SELECT TICKER, NUMMTGS FROM comp_execucomp.codirfin WHERE YEAR BETWEEN '{lastyear}' AND '{thisyear}' AND TICKER IN {SP500List}")))
 
-#output_excel_file(DirectorsUS, 'rmdirectors.xlsx')
-#output_excel_file(NumCommittees, 'rmdirectors2.xlsx')
+  
+
+  #%Independent Directors & Shares held & CEO Duality & Number of committees & Voting type
+  DirectorsUS = pd.DataFrame(data=(db.raw_sql(f"SELECT TICKER, CLASSIFICATION, NUM_OF_SHARES, EMPLOYMENT_CEO, EMPLOYMENT_CHAIRMAN, OWNLESS1, PCNT_CTRL_VOTINGPOWER FROM risk.rmdirectors WHERE YEAR BETWEEN '{lastyear}' AND '{thisyear}' AND TICKER IN {SP500List}")))
+
+
 
 
 SP500List = get_SP500_companies()
 today_date, year_ago_date, thisyear, lastyear = get_dates()
+count_committees()
+ceo = is_CEO_Dual()
 read_in_data_from_wrds()
-
+#output_excel_file(merged_data, 'mergedata.xlsx')
+#output_excel_file(ceo, 'ceoDuality.xlsx')
+dataframe = pd.DataFrame(data=(db.raw_sql(f"SELECT TICKER, EMPLOYMENT_CEO, EMPLOYMENT_CHAIRMAN, YEAR, meetingdate, NAME, FULLNAME FROM risk.rmdirectors WHERE YEAR BETWEEN '{lastyear}' AND '{thisyear}' AND TICKER IN {SP500List}")))
+print(dataframe)
+output_excel_file(dataframe, 'ceoDuality2.xlsx')
 
 
 end = time.time()
