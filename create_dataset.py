fcc5a0e497b79f3a2f3c82f38394e817a43d05b5
TobyWhittome <Toby.Whittome@outlook.com>
2024-01-18 16:47:27 +0000

    dates pulled out to update

--- create_dataset.py
+++ create_dataset.py
@@ -1,7 +1,20 @@
 import wrds
 import pandas as pd
 import os
+import time
+import datetime
+
+
+start = time.time()
+
 db = wrds.Connection(wrds_username="twhittome")
+
+def get_dates():
+  today = datetime.date.today()
+  thisYear = int(today.strftime("%Y"))
+  yearMod = thisYear - 1
+  modified_date = today.replace(year=yearMod)
+  return today, modified_date, thisYear, yearMod
 
 
 def get_SP500_companies() -> pd.DataFrame:
@@ -33,19 +46,28 @@ def read_in_data_from_wrds():
   WHERE g.YEAR BETWEEN 2023 AND 2024 AND g.TICKER IN {SP500List}
   '''
   merged_data = pd.DataFrame(data=(db.raw_sql(query)))
-  print(merged_data)
+  #print(merged_data)
 
   #Size (org summary) & Diversity ... need to add Annualreportdate BETWEEN 2023-01-01 AND 2024-01-15
-  OrgSummary = pd.DataFrame(data=(db.raw_sql(f'SELECT Ticker, NumberDirectors, GenderRatio, NationalityMix FROM boardex.na_wrds_org_summary WHERE TICKER IN {SP500List}')))
+  OrgSummary = pd.DataFrame(data=(db.raw_sql(f"SELECT Ticker, NumberDirectors, GenderRatio, NationalityMix, Annualreportdate FROM boardex.na_wrds_org_summary WHERE Annualreportdate BETWEEN '{year_ago_date}' AND '{today_date}' AND TICKER IN {SP500List}")))
+  #print(OrgSummary)
   #%Independent Directors & Shares held & CEO Duality & Number of committees & Voting type
-  DirectorsUS = pd.DataFrame(data=(db.raw_sql(f'SELECT TICKER, CLASSIFICATION, NUM_OF_SHARES, EMPLOYMENT_CEO, EMPLOYMENT_CHAIRMAN, AUDIT_MEMBERSHIP, CG_MEMBERSHIP, COMP_MEMBERSHIP, NOM_MEMBERSHIP, OWNLESS1, PCNT_CTRL_VOTINGPOWER FROM risk.rmdirectors WHERE YEAR BETWEEN 2023 AND 2024 AND TICKER IN {SP500List}')))
+  DirectorsUS = pd.DataFrame(data=(db.raw_sql(f"SELECT TICKER, CLASSIFICATION, NUM_OF_SHARES, EMPLOYMENT_CEO, EMPLOYMENT_CHAIRMAN, AUDIT_MEMBERSHIP, CG_MEMBERSHIP, COMP_MEMBERSHIP, NOM_MEMBERSHIP, OWNLESS1, PCNT_CTRL_VOTINGPOWER FROM risk.rmdirectors WHERE YEAR BETWEEN '{lastyear}' AND '{thisyear}' AND TICKER IN {SP500List}")))
   count_committees(DirectorsUS)
-  Governance = pd.DataFrame(data=(db.raw_sql(f'SELECT TICKER, DUALCLASS FROM risk.rmgovernance WHERE YEAR BETWEEN 2023 AND 2024 AND TICKER IN {SP500List}')))
-  Execucomp = pd.DataFrame(data=(db.raw_sql(f'SELECT TICKER, NUMMTGS FROM comp_execucomp.codirfin WHERE YEAR BETWEEN 2023 AND 2024 AND TICKER IN {SP500List}')))
+  Governance = pd.DataFrame(data=(db.raw_sql(f"SELECT TICKER, DUALCLASS FROM risk.rmgovernance WHERE YEAR BETWEEN '{lastyear}' AND '{thisyear}' AND TICKER IN {SP500List}")))
+  print(Governance)
+  Execucomp = pd.DataFrame(data=(db.raw_sql(f"SELECT TICKER, NUMMTGS FROM comp_execucomp.codirfin WHERE YEAR BETWEEN '{lastyear}' AND '{thisyear}' AND TICKER IN {SP500List}")))
 
 #output_excel_file(DirectorsUS, 'rmdirectors.xlsx')
 #output_excel_file(NumCommittees, 'rmdirectors2.xlsx')
 
 
 SP500List = get_SP500_companies()
+today_date, year_ago_date, thisyear, lastyear = get_dates()
 read_in_data_from_wrds()
+
+
+
+end = time.time()
+print("The time of execution of above program is :",
+      (end-start) * 10**3, "ms")
