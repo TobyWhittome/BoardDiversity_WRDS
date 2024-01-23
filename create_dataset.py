240d8a9ae51d3b4163c0ffb1fa8a62d9bf213756
TobyWhittome <Toby.Whittome@outlook.com>
2024-01-23 17:11:49 +0000

    refactor into class working

--- create_dataset.py
+++ create_dataset.py
@@ -7,9 +7,9 @@ import datetime
 class myData:
 
   def __init__(self):
-    SP500List, today_date, year_ago_date, thisyear, lastyear
+    self.SP500List, self.today_date, self.year_ago_date, self.thisyear, self.lastyear, self.db = None, None, None, None, None, None
 
-  def get_dates():
+  def get_dates(self):
     today = datetime.date.today()
     thisYear = int(today.strftime("%Y"))
     yearMod = thisYear - 1
@@ -17,27 +17,27 @@ class myData:
     return today, modified_date, thisYear, yearMod
 
 
-  def get_SP500_companies() -> pd.DataFrame:
+  def get_SP500_companies(self) -> pd.DataFrame:
     url = 'https://www.ssga.com/us/en/intermediary/etfs/library-content/products/fund-data/etfs/us/holdings-daily-us-en-spy.xlsx'
     frame = pd.read_excel(url, engine='openpyxl', usecols=['Ticker'], skiprows=4).dropna()
     return tuple(frame['Ticker'])
 
 
-  def output_excel_file(database, filename):
+  def output_excel_file(self, database, filename):
     excel_file_path = os.path.join(os.getcwd(), filename)
     database.to_excel(excel_file_path, index=False)
 
 
-  def count_committees():
-    dataframe = pd.DataFrame(data=(db.raw_sql(f"SELECT TICKER, AUDIT_MEMBERSHIP, CG_MEMBERSHIP, COMP_MEMBERSHIP, NOM_MEMBERSHIP FROM risk.rmdirectors WHERE YEAR BETWEEN '{lastyear}' AND '{thisyear}' AND TICKER IN {SP500List}")))
+  def count_committees(self):
+    dataframe = pd.DataFrame(data=(self.db.raw_sql(f"SELECT TICKER, AUDIT_MEMBERSHIP, CG_MEMBERSHIP, COMP_MEMBERSHIP, NOM_MEMBERSHIP FROM risk.rmdirectors WHERE YEAR BETWEEN '{self.lastyear}' AND '{self.thisyear}' AND TICKER IN {self.SP500List}")))
     membership_columns = ['audit_membership', 'cg_membership', 'comp_membership', 'nom_membership']
     total_df = dataframe.groupby('ticker')[membership_columns].apply(lambda x: (x.notna().sum() > 0).sum()).reset_index(name='total_memberships')
     return total_df
 
 
   #Has multiple companies which have more than one CEO shown.
-  def is_CEO_Dual():
-    dataframe = pd.DataFrame(data=(db.raw_sql(f"SELECT TICKER, EMPLOYMENT_CEO, EMPLOYMENT_CHAIRMAN FROM risk.rmdirectors WHERE YEAR BETWEEN '{lastyear}' AND '{thisyear}' AND TICKER IN {SP500List}")))
+  def is_CEO_Dual(self):
+    dataframe = pd.DataFrame(data=(self.db.raw_sql(f"SELECT TICKER, EMPLOYMENT_CEO, EMPLOYMENT_CHAIRMAN FROM risk.rmdirectors WHERE YEAR BETWEEN '{self.lastyear}' AND '{self.thisyear}' AND TICKER IN {self.SP500List}")))
     #dataframe = pd.DataFrame(data=(db.raw_sql(f"SELECT TICKER, EMPLOYMENT_CEO, EMPLOYMENT_CHAIRMAN, YEAR, meetingdate, NAME, FULLNAME FROM risk.rmdirectors WHERE YEAR BETWEEN '{lastyear}' AND '{thisyear}' AND TICKER IN {SP500List}")))
     com_data = []
     for index, row in dataframe.iterrows():
@@ -49,9 +49,9 @@ class myData:
     return pd.DataFrame(com_data)
 
 
-  def director_power():
+  def director_power(self):
     #%Independent Directors & Shares held & Number of committees & Voting type
-    dataframe = pd.DataFrame(data=(db.raw_sql(f"SELECT TICKER, CLASSIFICATION, NUM_OF_SHARES, OWNLESS1, PCNT_CTRL_VOTINGPOWER FROM risk.rmdirectors WHERE YEAR BETWEEN '{lastyear}' AND '{thisyear}' AND TICKER IN {SP500List}")))
+    dataframe = pd.DataFrame(data=(self.db.raw_sql(f"SELECT TICKER, CLASSIFICATION, NUM_OF_SHARES, OWNLESS1, PCNT_CTRL_VOTINGPOWER FROM risk.rmdirectors WHERE YEAR BETWEEN '{self.lastyear}' AND '{self.thisyear}' AND TICKER IN {self.SP500List}")))
     print(dataframe)
 
     
@@ -69,7 +69,7 @@ class myData:
 
 
 
-  def read_in_data_from_wrds():
+  def read_in_data_from_wrds(self):
 
     #Company Database
     query = f'''
@@ -77,14 +77,14 @@ class myData:
     FROM risk.rmgovernance g
     JOIN comp_execucomp.codirfin e 
     ON g.TICKER = e.TICKER AND e.YEAR = g.YEAR
-    WHERE g.YEAR BETWEEN '{lastyear}' AND '{thisyear}' AND g.TICKER IN {SP500List}
+    WHERE g.YEAR BETWEEN '{self.lastyear}' AND '{self.thisyear}' AND g.TICKER IN {self.SP500List}
     '''
-    merged_data = pd.DataFrame(data=(db.raw_sql(query)))
+    merged_data = pd.DataFrame(data=(self.db.raw_sql(query)))
     #print(merged_data)
 
     
     #Only has 84 rows instead of 500...
-    OrgSummary = pd.DataFrame(data=(db.raw_sql(f"SELECT Ticker, NumberDirectors, GenderRatio, NationalityMix, Annualreportdate FROM boardex.na_wrds_org_summary WHERE Annualreportdate BETWEEN '{year_ago_date}' AND '{today_date}' AND Ticker IN {SP500List}")))
+    OrgSummary = pd.DataFrame(data=(self.db.raw_sql(f"SELECT Ticker, NumberDirectors, GenderRatio, NationalityMix, Annualreportdate FROM boardex.na_wrds_org_summary WHERE Annualreportdate BETWEEN '{self.year_ago_date}' AND '{self.today_date}' AND Ticker IN {self.SP500List}")))
     newSummary = OrgSummary.drop_duplicates()
     #print(newSummary)
     
@@ -92,28 +92,37 @@ class myData:
     return merged_data
 
 
-  def combine_data(dataframe):
-    committees = myData.count_committees()
-    ceo = myData.is_CEO_Dual()
-    director_powerful = myData.director_power()
+  def combine_data(self, dataframe):
+    committees = self.count_committees()
+    ceo = self.is_CEO_Dual()
+    director_powerful = self.director_power()
     ceo_and_committees = pd.merge(committees, ceo, on='ticker', how='inner')
     total_dataset = pd.merge(ceo_and_committees, dataframe, on='ticker', how='inner')
     return total_dataset
 
 
-if __name__ == "__main__":
-  start = time.time()
-  db = wrds.Connection(wrds_username="twhittome")
 
-  SP500List = myData.get_SP500_companies()
-  today_date, year_ago_date, thisyear, lastyear = myData.get_dates()
-  dataframe = myData.read_in_data_from_wrds()
-  final_dataset = myData.combine_data(dataframe)
+def main():
+  start = time.time()
+  inst = myData()
+  inst.db = wrds.Connection(wrds_username="twhittome")
+  inst.SP500List = inst.get_SP500_companies()
+  inst.today_date, inst.year_ago_date, inst.thisyear, inst.lastyear = inst.get_dates()
+
+  dataframe = inst.read_in_data_from_wrds()
+  final_dataset = inst.combine_data(dataframe)
 
   #output_excel_file(final_dataset, 'orgstaff1.xlsx')
 
   end = time.time()
   print("The time of execution of above program is :",
         (end-start) * 10**3, "ms")
+  
+  return final_dataset
 
+
+
+
+if __name__ == "__main__":
+  print(main())
 
