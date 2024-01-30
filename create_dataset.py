97e70643ab234eb5f39e34fe76003c3dda75996e
TobyWhittome <Toby.Whittome@outlook.com>
2024-01-30 16:36:57 +0000

    CEO duality in prog

--- create_dataset.py
+++ create_dataset.py
@@ -7,7 +7,7 @@ import datetime
 class myData:
 
   def __init__(self):
-    self.SP500List, self.today_date, self.year_ago_date, self.thisyear, self.lastyear, self.db = None, None, None, None, None, None
+    self.SP500Tickers, self.today_date, self.year_ago_date, self.thisyear, self.lastyear, self.db, self.SP500IDs = None, None, None, None, None, None, None
 
   def get_dates(self):
     today = datetime.date.today()
@@ -21,6 +21,10 @@ class myData:
     url = 'https://www.ssga.com/us/en/intermediary/etfs/library-content/products/fund-data/etfs/us/holdings-daily-us-en-spy.xlsx'
     frame = pd.read_excel(url, engine='openpyxl', usecols=['Ticker'], skiprows=4).dropna()
     return tuple(frame['Ticker'])
+  
+  def get_SP500_IDs(self):
+    frame = pd.DataFrame(data=(self.db.raw_sql(f"SELECT DISTINCT boardid FROM boardex.na_wrds_company_profile WHERE ticker IN {self.SP500Tickers}")))
+    return tuple(frame['boardid'])
 
 
   def output_excel_file(self, database, filename):
@@ -29,7 +33,7 @@ class myData:
 
 
   def count_committees(self):
-    dataframe = pd.DataFrame(data=(self.db.raw_sql(f"SELECT TICKER, AUDIT_MEMBERSHIP, CG_MEMBERSHIP, COMP_MEMBERSHIP, NOM_MEMBERSHIP FROM risk.rmdirectors WHERE YEAR BETWEEN '{self.lastyear}' AND '{self.thisyear}' AND TICKER IN {self.SP500List}")))
+    dataframe = pd.DataFrame(data=(self.db.raw_sql(f"SELECT TICKER, AUDIT_MEMBERSHIP, CG_MEMBERSHIP, COMP_MEMBERSHIP, NOM_MEMBERSHIP FROM risk.rmdirectors WHERE YEAR BETWEEN '{self.lastyear}' AND '{self.thisyear}' AND TICKER IN {self.SP500Tickers}")))
     membership_columns = ['audit_membership', 'cg_membership', 'comp_membership', 'nom_membership']
     total_df = dataframe.groupby('ticker')[membership_columns].apply(lambda x: (x.notna().sum() > 0).sum()).reset_index(name='total_memberships')
     return total_df
@@ -37,26 +41,43 @@ class myData:
 
   #Has multiple companies which have more than one CEO shown.
   def is_CEO_Dual(self):
-    dataframe = pd.DataFrame(data=(self.db.raw_sql(f"SELECT TICKER, EMPLOYMENT_CEO, EMPLOYMENT_CHAIRMAN FROM risk.rmdirectors WHERE YEAR BETWEEN '{self.lastyear}' AND '{self.thisyear}' AND TICKER IN {self.SP500List}")))
+
+    CEO_list = 'Chairman/CEO', 'Chairman/President/CEO'
+    dataframeCEO = pd.DataFrame(data=(self.db.raw_sql(f"SELECT CompanyID, RoleName FROM boardex.na_wrds_dir_profile_emp WHERE DateEndRole BETWEEN '{self.year_ago_date}' AND '{self.today_date}' AND RoleName IN {CEO_list} AND CompanyID IN {self.SP500IDs}")))
+    dataframeCEO['rolename'].replace(to_replace=CEO_list, value=1, inplace=True)
+    print(dataframeCEO)
+    #now need the other 447 companies to have a zero in that place.
+    # -- this is not representitive as a percentage.
+
+
+    
+
+    dataframe = pd.DataFrame(data=(self.db.raw_sql(f"SELECT TICKER, EMPLOYMENT_CEO, EMPLOYMENT_CHAIRMAN FROM risk.rmdirectors WHERE YEAR BETWEEN '{self.lastyear}' AND '{self.thisyear}' AND TICKER IN {self.SP500Tickers}")))
     #dataframe = pd.DataFrame(data=(db.raw_sql(f"SELECT TICKER, EMPLOYMENT_CEO, EMPLOYMENT_CHAIRMAN, YEAR, meetingdate, NAME, FULLNAME FROM risk.rmdirectors WHERE YEAR BETWEEN '{lastyear}' AND '{thisyear}' AND TICKER IN {SP500List}")))
     com_data = []
+    ticker = 0
     for index, row in dataframe.iterrows():
       if row['employment_ceo'] == 'Yes':
         if row['employment_chairman'] == 'Yes':
           com_data.append({'ticker': row['ticker'], 'CEODuality': True})
+          ticker += 1
         else:
           com_data.append({'ticker': row['ticker'], 'CEODuality': False})
+    print(pd.DataFrame(com_data))
+    print(ticker)
+    #should be roughly 44%, so 327/790 = 41% -- is better
     return pd.DataFrame(com_data)
 
 
   def director_power(self):
     #%Independent Directors & Shares held & Number of committees & Voting type
-    dataframe = pd.DataFrame(data=(self.db.raw_sql(f"SELECT TICKER, CLASSIFICATION, NUM_OF_SHARES, OWNLESS1, PCNT_CTRL_VOTINGPOWER FROM risk.rmdirectors WHERE YEAR BETWEEN '{self.lastyear}' AND '{self.thisyear}' AND TICKER IN {self.SP500List}")))
+    dataframe = pd.DataFrame(data=(self.db.raw_sql(f"SELECT TICKER, CLASSIFICATION, NUM_OF_SHARES, OWNLESS1, PCNT_CTRL_VOTINGPOWER FROM risk.rmdirectors WHERE YEAR BETWEEN '{self.lastyear}' AND '{self.thisyear}' AND TICKER IN {self.SP500Tickers}")))
     #print(dataframe)
 
     #Legacy data unfort
-    dataframe2 = pd.DataFrame(data=(self.db.raw_sql(f"SELECT Ticker, sumaflin FROM block.block WHERE TICKER IN {self.SP500List}")))
-    print(dataframe2)
+    dataframe2 = pd.DataFrame(data=(self.db.raw_sql(f"SELECT Ticker, sumaflin FROM block.block WHERE TICKER IN {self.SP500Tickers}")))
+    #print(dataframe2)
+
     #Count if any directors have above 4.5% share individually. -- I need total company shares for a %..
     #Count percentage of company the board holds. - NUM_OF_SHARES
     #mtgdate BETWEEN '{self.year_ago_date}' AND '{self.today_date}' AND
@@ -81,13 +102,13 @@ class myData:
     FROM risk.rmgovernance g
     JOIN comp_execucomp.codirfin e 
     ON g.TICKER = e.TICKER AND e.YEAR = g.YEAR
-    WHERE g.YEAR BETWEEN '{self.lastyear}' AND '{self.thisyear}' AND g.TICKER IN {self.SP500List}
+    WHERE g.YEAR BETWEEN '{self.lastyear}' AND '{self.thisyear}' AND g.TICKER IN {self.SP500Tickers}
     '''
     merged_data = pd.DataFrame(data=(self.db.raw_sql(query)))
     #print(merged_data)
 
     #Only has 84 rows instead of 500...
-    OrgSummary = pd.DataFrame(data=(self.db.raw_sql(f"SELECT Ticker, NumberDirectors, GenderRatio, NationalityMix, Annualreportdate FROM boardex.na_wrds_org_summary WHERE Annualreportdate BETWEEN '{self.year_ago_date}' AND '{self.today_date}' AND Ticker IN {self.SP500List}")))
+    OrgSummary = pd.DataFrame(data=(self.db.raw_sql(f"SELECT Ticker, NumberDirectors, GenderRatio, NationalityMix, Annualreportdate FROM boardex.na_wrds_org_summary WHERE Annualreportdate BETWEEN '{self.year_ago_date}' AND '{self.today_date}' AND Ticker IN {self.SP500Tickers}")))
     newSummary = OrgSummary.drop_duplicates()
     #print(newSummary)
     
@@ -108,7 +129,8 @@ def main():
   start = time.time()
   inst = myData()
   inst.db = wrds.Connection(wrds_username="twhittome")
-  inst.SP500List = inst.get_SP500_companies()
+  inst.SP500Tickers = inst.get_SP500_companies()
+  inst.SP500IDs = inst.get_SP500_IDs()
   inst.today_date, inst.year_ago_date, inst.thisyear, inst.lastyear = inst.get_dates()
 
   dataframe = inst.read_in_data_from_wrds()
