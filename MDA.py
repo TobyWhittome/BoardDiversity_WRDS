5339ba39dfb356c34329ff5e51caacce11f9d689
TobyWhittome <toby.whittome@outlook.com>
2024-02-18 16:37:15 +0000

    MDA trying to get my data working

--- MDA.py
+++ MDA.py
@@ -2,25 +2,59 @@ from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 import create_dataset
 import pandas as pd
 import matplotlib as plt
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import sklearn
+from sklearn.preprocessing import StandardScaler, LabelEncoder
+from sklearn.model_selection import train_test_split
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score, confusion_matrix
 
 
-# Load your dataset
 df = create_dataset.main()
 
-# Let's assume that the last column is your target variable
-X = df.iloc[:, :-1].values
-y = df.iloc[:, -1].values
+X = df.iloc[:, 1:5].values
+print(X)
+y = df.iloc[:, 6].values
+print(y)
 
-# Create an LDA object
-lda = LinearDiscriminantAnalysis(n_components=2)  # Change n_components to the number of linear discriminants you want
+""" url = "https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data"
+cls = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'Class']
+dataset = pd.read_csv(url, names=cls)
+ 
+# divide the dataset into class and target variable
+X = dataset.iloc[:, 0:4].values
+print(X)
+y = dataset.iloc[:, 4].values
+print(y) """
 
-# Fit the model
+sc = StandardScaler()
+X = sc.fit_transform(X)
+le = LabelEncoder()
+y = le.fit_transform(y)
 
-X_r = lda.fit(X, y).transform(X)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+ 
+# apply Linear Discriminant Analysis
+lda = LinearDiscriminantAnalysis(n_components=2)
 
-# Now X_r contains the transformed data
-
-# Visualizing the transformed data
-plt.figure()
-plt.title('LDA of dataset')
-plt.show()
+X_train = lda.fit_transform(X_train, y_train)
+X_test = lda.transform(X_test)
+ 
+# plot the scatterplot
+plt.scatter(
+    X_train[:,0],X_train[:,1],c=y_train,cmap='rainbow',
+  alpha=0.7,edgecolors='b'
+)
+ 
+# classify using random forest classifier
+classifier = RandomForestClassifier(max_depth=2, random_state=0)
+classifier.fit(X_train, y_train)
+y_pred = classifier.predict(X_test)
+ 
+# print the accuracy and confusion matrix
+print('Accuracy : ' + str(accuracy_score(y_test, y_pred)))
+conf_m = confusion_matrix(y_test, y_pred)
+print(conf_m)
