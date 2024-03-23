8cad06430dc9fef96b3a2ca8ec22b0405cd2582c
TobyWhittome <Toby.Whittome@outlook.com>
2024-03-23 17:57:24 +0000

    cleanup

--- regression/factor_analysis.py
+++ regression/factor_analysis.py
@@ -7,46 +7,6 @@ import numpy as np
 from sklearn.linear_model import LinearRegression
 import seaborn as sns
 
-""" #Visualisation
-def create_heatmap(df, varifa, nonefa):
-    factors = 4
-
-    # a list of tuples containing titles for and instances of FactorAnalyzer class
-    fas = [
-        ("FA no rotation", nonefa),
-        ("FA varimax", varifa),
-    ]  
-
-    # Let's prepare some plots on one canvas (subplots)
-    fig, axes = plt.subplots(ncols=len(fas), figsize=(10, 8), squeeze=False)  # Ensure axes is always 2D
-    axes = axes.flatten()  # Flatten the axes array for easy iteration
-
-    for ax, (title, fa) in zip(axes, fas):
-        # Transpose the component (loading) matrix
-        factor_matrix = fa.loadings_  # Use loadings_ instead of components_
-        # Plot the data as a heat map
-        im = ax.imshow(factor_matrix, cmap="RdBu_r", vmax=1, vmin=-1)
-        # Add the corresponding value to the center of each cell
-        for (i, j), z in np.ndenumerate(factor_matrix):
-            ax.text(j, i, '{:0.2f}'.format(z), ha="center", va="center")
-        # Tell matplotlib about the metadata of the plot
-        ax.set_yticks(np.arange(len(df.columns)))
-        if ax is axes[0]:  # Check if it's the first subplot
-            ax.set_yticklabels(df.columns)
-        else:
-            ax.set_yticklabels([])
-        ax.set_title(title)
-        ax.set_xticks(np.arange(factors))
-        ax.set_xticklabels(["Factor {}".format(i+1) for i in range(factors)])
-    # Adjust layout
-    plt.tight_layout()
-
-    # Add a colorbar
-    cb = fig.colorbar(im, ax=axes, location='right', shrink=0.6, label="Loadings")
-    # Show the plot
-    plt.show() """
-
-
 def create_heatmap(df, varifa):
     factors = 4
 
@@ -83,7 +43,7 @@ def create_heatmap(df, varifa):
     plt.show()
 
 
-df = pd.read_excel('dataset/transformed_dataset.xlsx')
+df = pd.read_excel('dataset/final_dataset.xlsx')
 df.drop(columns=['ticker'], inplace=True)
 #df.drop(columns=['mktcapitalisation'], inplace=True)
 #create a 1D array of Tobin's Q
@@ -91,7 +51,6 @@ tobins_q = df['tobinsQ']
 df.drop(columns=['tobinsQ'], inplace=True)
 #df.drop(columns=['genderratio'], inplace=True)
 
-print("hello")
 kmo_all,kmo_model=calculate_kmo(df)
 print(kmo_model)
 
